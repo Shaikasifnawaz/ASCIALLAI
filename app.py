5d64c4e2d7d8d614a406aed1cdb173b7ef8a168e
Shaikasifnawaz <asifnawazaddy@gmail.com>
2024-12-03 18:12:15 +0530

    second commit

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def load_pdfs_endpoint():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
